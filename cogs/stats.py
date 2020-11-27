d0415e809d3cd8404d72d779b81a0652f83cc03d
unknown <joshuadkane@me.com>
2020-11-27 17:10:20 -0500

    member join and leave

--- cogs/stats.py
+++ cogs/stats.py
@@ -21,13 +21,23 @@ class Stats(commands.Cog):
 
     @commands.command()
     async def members(self, ctx):
-        embed=discord.Embed(title="Jarvis:", color=0x00ff40)
+        embed=discord.Embed(title="Jarvis:", color=0x63d0f7)
         embed.add_field(
             name="Amount of members", 
             value=f"There are curently {len(self.guild.members)} members in the server", 
             inline=True
         )
         await ctx.send(embed=embed)
+    
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        for channel in self.guild.channels:
+            if channel.id == 781581365513945121:
+                await channel.send(f'{member.name} Has left the server. <@!{member.id}>')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await member.send(f"Hi, welcome to {self.guild.name}! Please read the rules and agree to them. To agree react with 👍. Some parts of the server you need roles to access. Don't worry, you can get them by reacting to the message under the rules!")
 
 def setup(bot):
     bot.add_cog(Stats(bot))
